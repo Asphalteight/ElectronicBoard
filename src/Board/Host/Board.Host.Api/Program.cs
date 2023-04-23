a330fac12876e79ce317be3ff57c8445860393ef
Nikolay <nikolaynekr.17@gmail.com>
2023-04-23 16:36:42 +0300

    Миграции, docker и Program

--- src/Board/Host/Board.Host.Api/Program.cs
+++ src/Board/Host/Board.Host.Api/Program.cs
@@ -1,3 +1,4 @@
+using System.Text;
 using AutoMapper;
 using Board.Application.AppData.Context.Account.Repositories;
 using Board.Application.AppData.Context.Account.Services;
@@ -7,6 +8,10 @@ using Board.Application.AppData.Context.Category.Repositories;
 using Board.Application.AppData.Context.Category.Services;
 using Board.Application.AppData.Context.Comment.Repositories;
 using Board.Application.AppData.Context.Comment.Services;
+using Board.Application.AppData.Context.File.Repositories;
+using Board.Application.AppData.Context.File.Services;
+using Board.Application.AppData.Context.ImageKit.Repositories;
+using Board.Application.AppData.Context.ImageKit.Services;
 using Board.Application.AppData.Context.Message.Repositories;
 using Board.Application.AppData.Context.Message.Services;
 using Board.Application.AppData.Context.Subcategory.Repositories;
@@ -17,12 +22,17 @@ using Board.Infrastructure.DataAccess.Contexts.Account.Repository;
 using Board.Infrastructure.DataAccess.Contexts.Advertisement.Repository;
 using Board.Infrastructure.DataAccess.Contexts.Category.Repository;
 using Board.Infrastructure.DataAccess.Contexts.Comment.Repository;
+using Board.Infrastructure.DataAccess.Contexts.ImageKit.Repository;
 using Board.Infrastructure.DataAccess.Contexts.Message.Repository;
 using Board.Infrastructure.DataAccess.Contexts.Subcategory.Repository;
+using Board.Infrastructure.DataAccess.File.Repository;
 using Board.Infrastructure.DataAccess.Interfaces;
 using Board.Infrastructure.MapProfiles;
 using Board.Infrastructure.Repository;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -36,6 +46,9 @@ builder.Services.AddDbContext<BoardDbContext>((Action<IServiceProvider, DbContex
 
 builder.Services.AddScoped((Func<IServiceProvider, DbContext>) (sp => sp.GetRequiredService<BoardDbContext>()));
 
+// Add automapper
+builder.Services.AddSingleton<IMapper>(new Mapper(GetMapperConfiguration()));
+
 // Add repositories to the container.
 builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
 builder.Services.AddScoped<IAccountRepository, AccountRepository>();
@@ -44,6 +57,8 @@ builder.Services.AddScoped<ISubcategoryRepository, SubcategoryRepository>();
 builder.Services.AddScoped<IAdvertisementRepository, AdvertisementRepository>();
 builder.Services.AddScoped<ICommentRepository, CommentRepository>();
 builder.Services.AddScoped<IMessageRepository, MessageRepository>();
+builder.Services.AddScoped<IFileRepository, FileRepository>();
+builder.Services.AddScoped<IImageKitRepository, ImageKitRepository>();
 
 // Add services to the container.
 builder.Services.AddScoped<IAccountService, AccountService>();
@@ -52,16 +67,76 @@ builder.Services.AddScoped<ISubcategoryService, SubcategoryService>();
 builder.Services.AddScoped<IAdvertisementService, AdvertisementService>();
 builder.Services.AddScoped<ICommentService, CommentService>();
 builder.Services.AddScoped<IMessageService, MessageService>();
-
-// Add automapper
-builder.Services.AddSingleton<IMapper>(new Mapper(GetMapperConfiguration()));
+builder.Services.AddScoped<IFileService, FileService>();
+builder.Services.AddScoped<IImageKitService, ImageKitService>();
 
 builder.Services.AddControllers();
+
+// Add Auth & JWT
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        var key = builder.Configuration["JWT:Key"];
+        
+        options.RequireHttpsMetadata = false;
+        options.SaveToken = true;
+        options.TokenValidationParameters = new TokenValidationParameters()
+        {
+            ValidateActor = false,
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            ValidateIssuerSigningKey = true,
+            ValidateLifetime = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key!))
+        };
+    });
+
+// builder.Services.AddAuthorization(options =>
+// {
+//     options.AddPolicy("Client", policy =>
+//     {
+//         policy.RequireRole("Admin");
+//         policy.RequireClaim("", "");
+//     });
+// });
+builder.Services.AddAuthorization();
+
+builder.Services.TryAddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(options =>
 {
-    options.SwaggerDoc("v1", new OpenApiInfo { Title = "Advert Api", Version = "V1" });
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = "Заголовок авторизации по JWT используя схему барьера." +
+                      "Отправьте 'Bearer' [пробел] и затем ваш токен." +
+                      "Пример: 'Bearer key'",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = JwtBearerDefaults.AuthenticationScheme
+    });
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme()
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                },
+                Scheme = "oauth2",
+                Name = "Bearer",
+                In = ParameterLocation.Header
+            },
+            new List<string>()
+        }
+    });
+    
+    options.SwaggerDoc("v1", new OpenApiInfo { Title = "El Board Api", Version = "V1" });
     options.IncludeXmlComments(Path.Combine(Path.Combine(AppContext.BaseDirectory, $"{typeof(CreateAccountDto).Assembly.GetName().Name}.xml")));
     options.IncludeXmlComments(Path.Combine(Path.Combine(AppContext.BaseDirectory, "Documentation.xml")));
 });
@@ -76,6 +151,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
@@ -93,6 +170,8 @@ static MapperConfiguration GetMapperConfiguration()
         cfg.AddProfile<SubcategoryProfile>();
         cfg.AddProfile<CommentProfile>();
         cfg.AddProfile<MessageProfile>();
+        cfg.AddProfile<FileProfile>();
+        cfg.AddProfile<ImageKitProfile>();
     });
     configuration.AssertConfigurationIsValid();
     return configuration;
---
+++ src/Board/Host/Board.Host.Migrator/Migrations/20230415174918_Files_Images.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Board.Host.Migrator.Migrations
+{
+    /// <inheritdoc />
+    public partial class Files_Images : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Address",
+                table: "Advertisements",
+                type: "character varying(250)",
+                maxLength: 250,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Accounts",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.CreateTable(
+                name: "Files",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: false),
+                    Content = table.Column<byte[]>(type: "bytea", nullable: false),
+                    ContentType = table.Column<string>(type: "character varying(256)", maxLength: 256, nullable: false),
+                    Length = table.Column<int>(type: "integer", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Files", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ImageKits",
+                columns: table => new
+                {
+                    AdvertisementId = table.Column<int>(type: "integer", nullable: false),
+                    FirstImageId = table.Column<Guid>(type: "uuid", nullable: true),
+                    SecondImageId = table.Column<Guid>(type: "uuid", nullable: true),
+                    ThirdImageId = table.Column<Guid>(type: "uuid", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ImageKits", x => x.AdvertisementId);
+                    table.ForeignKey(
+                        name: "FK_ImageKits_Advertisements_AdvertisementId",
+                        column: x => x.AdvertisementId,
+                        principalTable: "Advertisements",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Files");
+
+            migrationBuilder.DropTable(
+                name: "ImageKits");
+
+            migrationBuilder.DropColumn(
+                name: "Address",
+                table: "Advertisements");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Accounts");
+        }
+    }
+}

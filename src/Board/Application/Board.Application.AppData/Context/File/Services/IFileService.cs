eb8fad0585690021006ca7365b8c4c833473ace2
Nikolay <nikolaynekr.17@gmail.com>
2023-04-23 16:30:05 +0300

    Добавлена возможность работы с файлами

---
+++ src/Board/Application/Board.Application.AppData/Context/File/Services/IFileService.cs
@@ -0,0 +1,42 @@
+﻿using Board.Contracts.Contexts.Advertisements;
+using Board.Contracts.File;
+using Board.Contracts.Files;
+
+namespace Board.Application.AppData.Context.File.Services;
+
+/// <summary>
+/// Сервис для работы с файлами.
+/// </summary>
+public interface IFileService
+{
+    /// <summary>
+    /// Получение информации о файле по его идентификатору.
+    /// </summary>
+    /// <param name="id">Идентификатор файла.</param>
+    /// <param name="cancellationToken">Токен отмены.</param>
+    /// <returns>Информация о файле.</returns>
+    Task<FileInfoDto> GetInfoByIdAsync(Guid id, CancellationToken cancellationToken);
+
+    /// <summary>
+    /// Загрузка файла в систему.
+    /// </summary>
+    /// <param name="model">Модель файла.</param>
+    /// <param name="cancellationToken">Токен отмены.</param>
+    /// <returns>Идентификатор загруженного файла.</returns>
+    Task<Guid> UploadAsync(FileDto model, CancellationToken cancellationToken);
+
+    /// <summary>
+    /// Скачивание файла.
+    /// </summary>
+    /// <param name="id">Идентификатор файла.</param>
+    /// <param name="cancellationToken">Токен отмены.</param>
+    /// <returns>Информация о скачиваемом файле.</returns>
+    Task<FileDto> DownloadAsync(Guid id, CancellationToken cancellationToken);
+
+    /// <summary>
+    /// Удаление файла по его идентификатору.
+    /// </summary>
+    /// <param name="id">Идентификатор файла.</param>
+    /// <param name="cancellationToken">Токен отмены.</param>        
+    Task DeleteAsync(Guid id, CancellationToken cancellationToken);
+}
---
+++ src/Board/Infrastructure/Board.Infrastructure.DataAccess/File/Repository/FileRepository.cs
@@ -0,0 +1,62 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Board.Application.AppData.Context.File.Repositories;
+using Board.Contracts.File;
+using Board.Contracts.Files;
+using Board.Domain.File;
+using Board.Infrastructure.Repository;
+using Microsoft.EntityFrameworkCore;
+
+namespace Board.Infrastructure.DataAccess.File.Repository
+{
+    /// <inheritdoc cref="IFileRepository"/>
+    public class FileRepository : IFileRepository
+    {
+        private readonly IRepository<Files> _repository;
+        private readonly IMapper _mapper;
+
+        /// <summary>
+        /// Инициализация экземпляра <see cref="FileRepository"/>.
+        /// </summary>
+        public FileRepository(IRepository<Files> repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        /// <inheritdoc/>
+        public async Task DeleteAsync(Guid id, CancellationToken cancellationToken)
+        {
+            var file = await _repository.GetByIdAsync(id, cancellationToken);
+            if (file == null)
+            {
+                return;
+            }
+
+            await _repository.DeleteAsync(file, cancellationToken);
+        }
+
+        /// <inheritdoc/>
+        public Task<FileDto?> DownloadAsync(Guid id, CancellationToken cancellationToken)
+        {
+            return _repository.GetAll().Where(x => x.Id == id)
+                              .ProjectTo<FileDto>(_mapper.ConfigurationProvider)
+                              .FirstOrDefaultAsync(cancellationToken);
+        }
+
+        /// <inheritdoc/>
+        public Task<FileInfoDto?> GetInfoByIdAsync(Guid id, CancellationToken cancellationToken)
+        {
+            return _repository.GetAll().Where(x => x.Id == id)
+                              .ProjectTo<FileInfoDto>(_mapper.ConfigurationProvider)
+                              .FirstOrDefaultAsync(cancellationToken);
+        }
+
+        /// <inheritdoc/>
+        public async Task<Guid> UploadAsync(Files model, CancellationToken cancellationToken)
+        {
+            await _repository.AddAsync(model, cancellationToken);
+            return model.Id;
+        }
+    }
+}

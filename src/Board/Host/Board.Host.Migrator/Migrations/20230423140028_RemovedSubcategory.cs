e4363fa909692d6dcabdb2234757514cd47597f9
Nikolay <nikolaynekr.17@gmail.com>
2023-04-23 17:16:11 +0300

    --- миграция к прошлому коммиту

---
+++ src/Board/Host/Board.Host.Migrator/Migrations/20230423140028_RemovedSubcategory.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Board.Host.Migrator.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemovedSubcategory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Advertisements_Subcategories_SubcategoryId",
+                table: "Advertisements");
+
+            migrationBuilder.DropTable(
+                name: "Subcategories");
+
+            migrationBuilder.RenameColumn(
+                name: "SubcategoryId",
+                table: "Advertisements",
+                newName: "CategoryId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Advertisements_SubcategoryId",
+                table: "Advertisements",
+                newName: "IX_Advertisements_CategoryId");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ParentCategoryId",
+                table: "Categories",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Advertisements_Categories_CategoryId",
+                table: "Advertisements",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Advertisements_Categories_CategoryId",
+                table: "Advertisements");
+
+            migrationBuilder.DropColumn(
+                name: "ParentCategoryId",
+                table: "Categories");
+
+            migrationBuilder.RenameColumn(
+                name: "CategoryId",
+                table: "Advertisements",
+                newName: "SubcategoryId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Advertisements_CategoryId",
+                table: "Advertisements",
+                newName: "IX_Advertisements_SubcategoryId");
+
+            migrationBuilder.CreateTable(
+                name: "Subcategories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    CategoryId = table.Column<int>(type: "integer", nullable: false),
+                    Name = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Subcategories", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Subcategories_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Subcategories_CategoryId",
+                table: "Subcategories",
+                column: "CategoryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Advertisements_Subcategories_SubcategoryId",
+                table: "Advertisements",
+                column: "SubcategoryId",
+                principalTable: "Subcategories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}

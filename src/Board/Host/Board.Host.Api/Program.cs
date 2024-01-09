8e37e4012ef28a422e3200723bfa17b3fb75901b
Nikolay <nikolaynekr.17@gmail.com>
2024-01-10 00:01:09 +0300

    обновил до dotnet 8.0 и починил докер

--- src/Board/Host/Board.Host.Api/Program.cs
+++ src/Board/Host/Board.Host.Api/Program.cs
@@ -75,11 +75,8 @@ builder.Services.AddSwaggerModule();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
